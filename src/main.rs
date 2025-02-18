c71f0bc39054d86abf00af337f9b8fd5dd7edb96
Brian Hicks <brian@brianthicks.com>
2025-02-18 09:38:16 -0600

    get the config

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,40 @@
+use std::path::PathBuf;
+
 mod config;
 mod dates;
 mod task;
 mod taskwarrior;
 
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct App {
+    #[clap(long, default_value = "task")]
+    task_bin: PathBuf,
+}
+
+impl App {
+    async fn run(&self) -> Result<()> {
+        let tw = taskwarrior::Taskwarrior::new(self.task_bin.clone());
+
+        let config = tw
+            .config()
+            .await
+            .context("could not get taskwarrior config")?;
+
+        println!("{:#?}", config);
+
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let app = App::parse();
+
+    if let Err(err) = app.run().await {
+        eprintln!("{:#}", err);
+        std::process::exit(1);
+    }
 }
