e447e9770cdb1740cce5af6c26052a30d3ef6dcd
Brian Hicks <brian@brianthicks.com>
2025-02-18 17:38:24 -0600

    make the styling all work together

--- src/app.rs
+++ src/app.rs
@@ -8,7 +8,7 @@ use itertools::Itertools;
 use rand::prelude::*;
 use ratatui::{
     layout::{Alignment, Constraint, Direction, Flex, Layout},
-    style::{Style, Stylize},
+    style::{palette::tailwind, Style, Stylize},
     text::{Line, Span, Text},
     widgets::{Gauge, Paragraph, Wrap},
     Frame,
@@ -46,10 +46,16 @@ impl App {
         let [app_box_area] = app_box_vert.areas(app_area);
         let [app_box_area] = app_box_horiz.areas(app_box_area);
 
-        match &self.doing {
-            Activity::Nothing => {
-                frame.render_widget("Nothing to do right now.", app_area);
-            }
+        let gauge_style = Style::new().fg(tailwind::BLUE.c800).bg(tailwind::BLUE.c400);
+
+        let (title, gauge) = match &self.doing {
+            Activity::Nothing => (
+                Paragraph::new(Text::from("Nothing to do right now")),
+                Gauge::default()
+                    .label("0:00")
+                    .ratio(100.0)
+                    .use_unicode(true),
+            ),
             Activity::Task {
                 task,
                 started,
@@ -80,28 +86,64 @@ impl App {
                     sections.push(Span::from(project));
                 }
 
-                let title = Paragraph::new(Line::from(sections))
-                    .centered()
-                    .wrap(Wrap { trim: false });
+                sections.push(Span::from(" "));
+                sections.push(Span::styled(
+                    format!(
+                        "until {}",
+                        (*started + *length)
+                            .with_timezone(&Local)
+                            .format("%-I:%M %P")
+                    ),
+                    Style::default().italic().dim(),
+                ));
 
-                let gauge = Gauge::default()
-                    .label(format!(
-                        "{}:{:02}",
-                        time_remaining.num_minutes(),
-                        time_remaining.num_seconds() % 60
-                    ))
-                    .ratio(percent_remaining)
-                    .use_unicode(true);
-
-                let [title_area, gauge_area] =
-                    Layout::vertical([Constraint::Fill(1), Constraint::Length(3)])
-                        .areas(app_box_area);
-
-                frame.render_widget(title, title_area);
-                frame.render_widget(gauge, gauge_area);
+                (
+                    Paragraph::new(Line::from(sections))
+                        .centered()
+                        .wrap(Wrap { trim: false }),
+                    Gauge::default()
+                        .label(format!(
+                            "{}:{:02}",
+                            time_remaining.num_minutes(),
+                            time_remaining.num_seconds() % 60
+                        ))
+                        .gauge_style(gauge_style)
+                        .ratio(percent_remaining)
+                        .use_unicode(true),
+                )
             }
-            Activity::Break { .. } => frame.render_widget("taking a break", app_area),
-        }
+            Activity::Break { started, length } => {
+                let time_remaining = *length - (Utc::now() - started);
+
+                let percent_remaining =
+                    time_remaining.num_seconds() as f64 / length.num_seconds() as f64;
+
+                (
+                    Paragraph::new(format!(
+                        "Taking a break until {}",
+                        (*started + *length)
+                            .with_timezone(&Local)
+                            .format("%-I:%M %p")
+                    ))
+                    .centered(),
+                    Gauge::default()
+                        .label(format!(
+                            "{}:{:02}",
+                            time_remaining.num_minutes(),
+                            time_remaining.num_seconds() % 60
+                        ))
+                        .gauge_style(gauge_style)
+                        .ratio(percent_remaining)
+                        .use_unicode(true),
+                )
+            }
+        };
+
+        let [title_area, gauge_area] =
+            Layout::vertical([Constraint::Fill(1), Constraint::Length(3)]).areas(app_box_area);
+
+        frame.render_widget(title, title_area);
+        frame.render_widget(gauge, gauge_area);
 
         frame.render_widget(
             Line::from(vec![
@@ -115,8 +157,7 @@ impl App {
                 Span::from(" extend time"),
             ])
             .alignment(Alignment::Center)
-            .white()
-            .on_blue(),
+            .style(gauge_style.reversed()),
             status_line_area,
         );
     }
